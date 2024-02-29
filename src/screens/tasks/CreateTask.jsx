a30e35c93d97997aefcb135219db175cad2a4daa
Sabrina Huber <huber_sabrina@me.com>
2024-02-29 16:19:26 +0100

    add documentation to create task screen

--- src/screens/tasks/CreateTask.jsx
+++ src/screens/tasks/CreateTask.jsx
@@ -8,70 +8,92 @@ import CustomBoxButton from "../../components/buttons/CustomBoxButton";
 import {ICONS} from "../../constants/icons";
 import SelectListModal from "../../components/modals/SelectListModal";
 
-function CreateList({navigation}){
-    const { theme } = useTheme();
+//ACHTUNG: Hier wäre optional super, wenn wir keinen Speichern und Abbrechen Button benötigen würden
+//und das stattdessen mit der Tastatur lösen könnten - leider ist das bis jetzt noch nicht möglich
+
+function CreateList({navigation}) {
+    const {theme} = useTheme();
     const isDarkMode = theme === DARKMODE;
 
-    const bottomSheetRef = useRef(null);
+    // Reference for the select list modal
+    const selectListModalRef = useRef(null);
 
-    const handlePresentModalPress = () => bottomSheetRef.current?.present();
-
+    // State variables for the title, notes, and selected list
     const [title, setTitle] = useState('');
     const [notes, setNotes] = useState('');
-    const [selectedList, setSelectedList] = useState("Ingenium"); // Beispielinitialisierung
+    const [selectedList, setSelectedList] = useState("Ingenium");
 
+    /*
+    * Function to handle pressing the select list modal button
+    * When the user clicks on the "Liste" button, this function is triggered,
+    * which opens the select list modal.
+    */
+    const handlePresentModalPress = () => selectListModalRef.current?.present();
+
+    /*
+    * Function to handle the selection of a list from the modal
+    * When a list is selected from the modal, this function is called.
+    * It updates the selectedList state with the chosen list name,
+    * and then dismisses the modal.
+    */
     const handleListSelection = (buttonTextLeft) => {
         setSelectedList(buttonTextLeft);
-        console.log("listname: " + buttonTextLeft)
-        bottomSheetRef.current?.dismiss(); // Schließt das Modal
+        //console.log("list name: " + buttonTextLeft)
+        selectListModalRef.current?.dismiss(); // Closes the modal
     };
 
+    // Function to navigate to the edit task details screen
     const navigateToEditTaskDetails = () => {
-        Keyboard.dismiss(); // Schließt die Tastatur
-        navigation.navigate("EditTaskDetails_Screen", {
-            title: title, // Übergabe des Titels
-            notes: notes, // Übergabe der Notizen
-        });
+        // Dismiss the keyboard and navigate to the edit task details screen
+        Keyboard.dismiss();
+        navigation.navigate("EditTaskDetails_Screen");
+        // Optional: Pass additional props like title and notes to the EditTaskDetails screen for further use
+        // navigation.navigate("EditTaskDetails_Screen", {title, notes});
     };
     const handleGoBack = () => {
-        navigation.goBack(); // goBack() aufrufen, wenn der Button gedrückt wird
+        navigation.goBack();
     };
 
     return (
         <View style={isDarkMode ? styles.containerDark : styles.containerLight}>
+            {/* Custom back button */}
             <CustomBackButton onPress={handleGoBack}/>
             <View style={isDarkMode ? styles.contentDark : styles.contentLight}>
+                {/* Header */}
                 <Text style={[isDarkMode ? styles.textDark : styles.textLight, styles.header]}>
                     Aufgabe hinzufügen
                 </Text>
-
+                {/* Spacing */}
                 <View style={styles.spacing}></View>
-                    <View style={isDarkMode ? styles.textInputBoxesDark : styles.textInputBoxesLight}>
-                        <View style={[isDarkMode ? styles.textInputBoxDark : styles.textInputBoxLight,
-                                      isDarkMode ? styles.borderDark : styles.borderLight]}>
-                            <TextInput
-                                placeholder={"Titel"}
-                                placeholderTextColor={isDarkMode ? DARKMODE.PLACEHOLDER_TEXTCOLOR : LIGHTMODE.PLACEHOLDER_TEXTCOLOR}
-                                style={isDarkMode ? styles.inputDark : styles.inputLight}
-                                selectionColor={isDarkMode ? DARKMODE.CURSOR_COLOR : LIGHTMODE.CURSOR_COLOR}
-                                onChangeText={(text) => setTitle(text)}
-                            />
-                        </View>
-                        <View style={isDarkMode ? styles.textInputBoxNotesDark : styles.textInputBoxNotesLight}>
-                            <TextInput placeholder={"Notizen"}
-                                       placeholderTextColor={isDarkMode ? DARKMODE.PLACEHOLDER_TEXTCOLOR : LIGHTMODE.PLACEHOLDER_TEXTCOLOR}
-                                       style={isDarkMode ? styles.inputDarkNotes : styles.inputLightNotes}
-                                       selectionColor={isDarkMode ? DARKMODE.CURSOR_COLOR : LIGHTMODE.CURSOR_COLOR}
-                                       maxLength={1000}
-                                       multiline={true}
-                                       scrollEnabled={true}
-                                       onChangeText={(notes) => setNotes(notes)}
-                                       value={notes}
-                            />
-                        </View>
+                {/* Input fields for title and notes */}
+                <View style={isDarkMode ? styles.textInputBoxesDark : styles.textInputBoxesLight}>
+                    <View style={[styles.textInputBox,
+                        isDarkMode ? styles.borderDark : styles.borderLight]}>
+                        <TextInput
+                            placeholder={"Titel"}
+                            placeholderTextColor={isDarkMode ? DARKMODE.PLACEHOLDER_TEXTCOLOR : LIGHTMODE.PLACEHOLDER_TEXTCOLOR}
+                            style={isDarkMode ? styles.inputDark : styles.inputLight}
+                            selectionColor={isDarkMode ? DARKMODE.CURSOR_COLOR : LIGHTMODE.CURSOR_COLOR}
+                            onChangeText={(title) => setTitle(title)}
+                            value={title}
+                        />
                     </View>
-
+                    <View style={isDarkMode ? styles.textInputBoxNotesDark : styles.textInputBoxNotesLight}>
+                        <TextInput placeholder={"Notizen"}
+                                   placeholderTextColor={isDarkMode ? DARKMODE.PLACEHOLDER_TEXTCOLOR : LIGHTMODE.PLACEHOLDER_TEXTCOLOR}
+                                   style={isDarkMode ? styles.inputDarkNotes : styles.inputLightNotes}
+                                   selectionColor={isDarkMode ? DARKMODE.CURSOR_COLOR : LIGHTMODE.CURSOR_COLOR}
+                                   maxLength={1000}
+                                   multiline={true}
+                                   scrollEnabled={true}
+                                   onChangeText={(notes) => setNotes(notes)}
+                                   value={notes}
+                        />
+                    </View>
+                </View>
+                {/* Spacing */}
                 <View style={styles.spacing}></View>
+                {/* Custom box buttons for selecting list and navigating to edit task details */}
                 <View>
                     <CustomBoxButton
                         buttonTextLeft={"Liste"}
@@ -95,19 +117,20 @@ function CreateList({navigation}){
                         extraPadding={10}
                     />
                 </View>
-
+                {/* Optional buttons for saving and canceling */}
                 <View style={styles.buttonBox}>
-                    <View  style={styles.buttonOne}>
-                        <CustomButton title={"Speichern"} onPressFunction={()=> console.log("Speichern gedrückt")}/>
+                    <View style={styles.buttonOne}>
+                        <CustomButton title={"Speichern"} onPressFunction={() => console.log("Speichern gedrückt")}/>
                     </View>
-                    <View  style={styles.buttonTwo}>
-                        <CustomButton title={"Abbrechen"} onPressFunction={()=> handleGoBack()}/>
+                    <View style={styles.buttonTwo}>
+                        <CustomButton title={"Abbrechen"} onPressFunction={() => handleGoBack()}/>
                     </View>
                 </View>
             </View>
+            {/* Select list modal */}
             <SelectListModal
-                ref={bottomSheetRef}
-                onSelect={handleListSelection} // Prop für die Callback-Funktion
+                ref={selectListModalRef}
+                onSelect={handleListSelection} // Prop for the callback function
             />
         </View>
     )
@@ -116,6 +139,7 @@ function CreateList({navigation}){
 export default CreateList;
 
 const styles = StyleSheet.create({
+    // Container styles for light and dark mode
     containerLight: {
         flex: 1,
         backgroundColor: LIGHTMODE.BACKGROUNDCOLOR,
@@ -126,6 +150,7 @@ const styles = StyleSheet.create({
         backgroundColor: DARKMODE.BACKGROUNDCOLOR,
         paddingHorizontal: SIZES.SPACING_HORIZONTAL_DEFAULT
     },
+    // Content styles for light and dark mode
     contentLight: {
         flex: 1,
         backgroundColor: LIGHTMODE.BACKGROUNDCOLOR,
@@ -136,11 +161,13 @@ const styles = StyleSheet.create({
         backgroundColor: DARKMODE.BACKGROUNDCOLOR,
         marginTop: SIZES.MARGIN_TOP_FROM_BACKBUTTON_HEADER
     },
+    // Header text styles
     header: {
         fontSize: SIZES.SCREEN_HEADER,
         fontWeight: SIZES.SCREEN_HEADER_WEIGHT,
         paddingBottom: 5,
     },
+    // Text styles for light and dark mode
     textLight: {
         color: LIGHTMODE.TEXT_COLOR,
         fontSize: SIZES.SCREEN_TEXT_NORMAL,
@@ -149,9 +176,11 @@ const styles = StyleSheet.create({
         color: DARKMODE.TEXT_COLOR,
         fontSize: SIZES.SCREEN_TEXT_NORMAL,
     },
+    // Spacing between elements
     spacing: {
         marginVertical: SIZES.SPACES_VERTICAL_BETWEEN_BOXES,
     },
+    // Text input box styles for light and dark mode
     textInputBoxesLight: {
         backgroundColor: LIGHTMODE.BOX_COLOR,
         borderRadius: SIZES.BORDER_RADIUS
@@ -160,18 +189,18 @@ const styles = StyleSheet.create({
         backgroundColor: DARKMODE.BOX_COLOR,
         borderRadius: SIZES.BORDER_RADIUS
     },
-    textInputBoxLight: {
+    // Title input box styles for light and dark mode
+    textInputBox: {
         paddingVertical: SIZES.SPACING_HORIZONTAL_DEFAULT,
     },
-    textInputBoxDark: {
-        paddingVertical: SIZES.SPACING_HORIZONTAL_DEFAULT
-    },
+    // Notes input box styles for light and dark mode
     textInputBoxNotesLight: {
         padding: SIZES.SPACING_HORIZONTAL_DEFAULT,
     },
     textInputBoxNotesDark: {
         padding: SIZES.SPACING_HORIZONTAL_DEFAULT
     },
+    // Border styles for light and dark mod
     borderLight: {
         borderBottomWidth: 1,
         borderColor: LIGHTMODE.BACKGROUNDCOLOR,
@@ -182,6 +211,7 @@ const styles = StyleSheet.create({
         borderColor: DARKMODE.BACKGROUNDCOLOR,
         marginHorizontal: SIZES.SPACING_HORIZONTAL_DEFAULT
     },
+    // Title input styles for light and dark mode
     inputLight: {
         fontSize: SIZES.TEXTINPUT_SIZE,
         color: LIGHTMODE.TEXTINPUT_COLOR,
@@ -192,6 +222,7 @@ const styles = StyleSheet.create({
         color: DARKMODE.TEXTINPUT_COLOR,
         textAlign: "left",
     },
+    // Notes input styles for light and dark mode
     inputLightNotes: {
         fontSize: SIZES.TEXTINPUT_SIZE,
         color: LIGHTMODE.TEXTINPUT_COLOR,
@@ -204,18 +235,18 @@ const styles = StyleSheet.create({
         textAlign: "left",
         height: 100,
     },
-    boxSpacing: {
-        marginRight: 15,
-    },
+    // Button container styles
     buttonBox: {
         flexDirection: "row",
         marginTop: 40
     },
+    // Save button styles
     buttonOne: {
         flex: 1,
         marginRight: 10,
         height: 50,
     },
+    // Cancel button styles
     buttonTwo: {
         flex: 1,
         height: 50,
