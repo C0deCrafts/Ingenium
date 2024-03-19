402bfeb29fea9d99dcf0e2dc0615664375e008df
Jelena <jelena_turk@protonmail.com>
2024-03-19 23:34:41 +0100

    - test the implemented components in the ProfileSettings Screen

--- src/screens/settings/ProfileSettings.jsx
+++ src/screens/settings/ProfileSettings.jsx
@@ -2,16 +2,36 @@ import {Text, View, StyleSheet} from "react-native";
 import CustomDrawerHeader from "../../components/buttons/CustomDrawerHeader";
 import {DARKMODE, LIGHTMODE} from "../../constants/styleSettings";
 import {useTheme} from "../../context/ThemeContext";
+import {useDatabase} from "../../context/DatabaseContext";
+import TaskTitleDateElement from "../../components/taskComponents/TaskTitleDateElement";
+
 
 function ProfileSettings({navigation}){
     const { theme } = useTheme();
     const isDarkMode = theme === DARKMODE;
+
+    const {tasks} = useDatabase();
 
     return (
         <View  style={isDarkMode ? styles.containerDark : styles.containerLight}>
             <CustomDrawerHeader title="Profil bearbeiten" onPress={()=> navigation.openDrawer()}/>
             <View style={isDarkMode ? styles.contentDark : styles.contentLight}>
                 <Text style={isDarkMode ? styles.textDark : styles.textLight}>Profil bearbeiten</Text>
+                {
+                    tasks.filter(task => !task.isDone).map(task => {
+                        return (
+                            <TaskTitleDateElement
+                                key={task.taskId}
+                                p_taskId={task.taskId}
+                                p_taskIsDone={task.isDone}
+                                taskTitle={task.taskTitle}
+                                isTaskTitlePreview={true}
+                                showDate={true}
+                                dateText={`The id of the task: ${task.taskId}`}
+                                taskIsInCompletedScreen={false}
+                            />
+                        )})
+                }
             </View>
         </View>
     )
