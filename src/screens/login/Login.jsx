2441daf8777f2c3b9f1a84253c6c3bd16d3f99ff
Jelena <jelena_turk@protonmail.com>
2024-03-23 12:08:15 +0100

    style adjustments login screen

--- src/screens/login/Login.jsx
+++ src/screens/login/Login.jsx
@@ -1,7 +1,7 @@
 import {Text, View, StyleSheet, TouchableOpacity, Image} from "react-native";
 import * as Linking from "expo-linking"
 import CustomButton from "../../components/buttons/CustomButton";
-import {COLOR, DARKMODE, LIGHTMODE, SIZES} from "../../constants/styleSettings";
+import {DARKMODE, LIGHTMODE, SIZES} from "../../constants/styleSettings";
 import {useTheme} from "../../context/ThemeContext";
 import {useSafeAreaInsets} from "react-native-safe-area-context";
 import CustomInputFieldLogin from "../../components/inputFields/CustomInputFieldLogin";
@@ -37,21 +37,22 @@ function Login({navigation}){
 
     return (
         <View style={isDarkMode ? styles.containerDark : styles.containerLight}>
+            {/*Image and Greeting*/}
             <View style={[styles.container, styles.paddingBottom]}>
-                <Image source={require("../../assets/images/ingenium_logo.png")} style={{width: "90%"}} resizeMode={"contain"}
-                />
+                <Image source={require("../../assets/images/Ingenium_Logo_with_box.png")} style={styles.logo} resizeMode={"contain"}/>
                 <Text style={[isDarkMode ? styles.textDark : styles.textLight, styles.textNormal]}>Willkommen!</Text>
                 <Text style={[isDarkMode ? styles.textDark : styles.textLight, styles.textNormal]}>Nutze deine ILIAS Zugangsdaten zur Anmeldung.</Text>
             </View>
+            {/*Input and Login Button*/}
             <View style={styles.inputFieldContainer}>
                 <CustomInputFieldLogin placeholder="Nutzername" keyboardType={"default"} maxTextInputLength={25} iconName={ICONS.LOGIN.USER}/>
                 <CustomInputFieldLogin placeholder="Passwort" keyboardType={"default"} isPassword={true} maxTextInputLength={25} iconName={ICONS.LOGIN.LOCK}/>
                 <CustomButton title={"Anmelden"} onPressFunction={()=> console.log("Login was Pressed")}/>
             </View>
-
+            {/*Forgot Password & Create Account*/}
             <View style={[styles.container, styles.paddingTop]}>
                 <TouchableOpacity onPress={()=>{forgotPassword()}}>
-                    <Text style={styles.textButton}>Password vergessen?</Text>
+                    <Text style={[styles.textButton, styles.textXS]}>Password vergessen?</Text>
                 </TouchableOpacity>
                 <View style={styles.footer}>
                     <Text style={[isDarkMode ? styles.textDark : styles.textLight, styles.textXS]}>Keinen Account?</Text>
@@ -76,16 +77,22 @@ function getStyles(insets) {
             backgroundColor: LIGHTMODE.BACKGROUNDCOLOR,
             paddingTop: insets.top,
             marginHorizontal: SIZES.SPACING_HORIZONTAL_DEFAULT,
+            justifyContent: "center"
         },
         containerDark: {
             flex: 1,
             backgroundColor: DARKMODE.BACKGROUNDCOLOR,
             paddingTop: insets.top,
             marginHorizontal: SIZES.SPACING_HORIZONTAL_DEFAULT,
+            justifyContent: "center"
         },
         container: {
             justifyContent: "center",
             alignItems: "center",
+        },
+        logo: {
+            width: '80%',
+            marginBottom: SIZES.SPACING_VERTICAL_DEFAULT
         },
         paddingBottom: {
             paddingBottom: 30
@@ -95,6 +102,7 @@ function getStyles(insets) {
         },
         textNormal: {
             fontSize: SIZES.TEXT_SIZE,
+            lineHeight: 28
         },
         textXS: {
             fontSize: SIZES.SCREEN_TEXT_SMALL,
@@ -108,7 +116,7 @@ function getStyles(insets) {
             textAlign: "center"
         },
         inputFieldContainer: {
-            rowGap: 20
+            rowGap: SIZES.SPACING_HORIZONTAL_DEFAULT,
         },
         textButton: {
             textDecorationLine: "underline",
